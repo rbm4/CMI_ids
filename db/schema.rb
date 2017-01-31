3ad8fca533d8e2510d461514422cfbef9d02ae84
Ricardo Malafaia <rbm4@cin.ufpe.br>
2017-01-31 13:14:53 +0000

    deploy para o heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170125152132) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "equipamentos", force: :cascade do |t|
     t.string   "tipo"
     t.string   "descricao"
